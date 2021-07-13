70d70989fcba1fc96488c376c0cdefb95aa6daf1
Andras <rohadtsajt@gmail.com>
2021-07-13 20:26:55 +0100

    mongodb extra line for google oath2 multiple account issues

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ passport.use(new GoogleStrategy({
     // console.log(profile);
     User.findOrCreate({
       googleId: profile.id,
-      username: "googleIdUser"
+      email: "googleIdUser"
     }, function(err, user) {
       return cb(err, user);
     });
