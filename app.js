cdf66e9b83eefd254ac8ce5c6ca49e912f604e7a
Andras <rohadtsajt@gmail.com>
2021-07-15 14:37:55 +0100

    maybe fix

--- app.js
+++ app.js
@@ -72,10 +72,10 @@ passport.use(new GoogleStrategy({
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
-    // console.log(profile);
+    console.log(profile);
+    
     User.findOrCreate({
       googleId: profile.id,
-      username: profile.name
     }, function(err, user) {
       return cb(err, user);
     });
